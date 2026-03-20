bd4a6429916179e21298082e96386d7936c48325
Talgat Ryshmanov <ryshman@gmail.com>
2026-03-20 15:23:03 -0400

    fix: address actionable PR comments (loop 1)

--- cmd/axym/main.go
+++ cmd/axym/main.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"context"
 	"os"
-	"os/signal"
-	"syscall"
 )
 
 var version = "dev"
 
 func main() {
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
-	defer stop()
-	os.Exit(executeContext(ctx, os.Args[1:], os.Stdout, os.Stderr))
+	os.Exit(execute(os.Args[1:], os.Stdout, os.Stderr))
 }
